1841b706c19a8e5614a84bf42eba5eee06e90bd3
edward <edwardlfish@gmail.com>
2025-10-06 14:05:14 +0300

    Add book input form to index.html and refactor script for book management

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
 const library = [];
 // isbn, title, author, year
-let inputData = prompt("Enter data in the format: ISBN, Title, Author, Year of publishing");
-while (inputData) {
-    // TODO create and add unique book to library
-    const arr = inputData.split(',');
-    if(findBook(library, arr[0] ) === -1){
-        const book = new Book(arr[0], arr[1], arr[2], arr[3]);
-        library.push(book);
-    }
-    inputData = prompt("Enter data in the format: ISBN, Title, Author, Year of publishing");
-}
+addBook.onclick = function () {
+    // TODO task 1 get data from inputs, and put book in <ol id="result"></ol>
+    // check is book unique
 
-printLibrary(library);
-
-function printLibrary(library) {
-    // TODO add all books from library to <ol id="result"></ol>
-    for (let i = 0; i < library.length; i++) {
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(library[i].toString()))
-        result.appendChild(li);
-    }
+    // TODO task 2 advanced: implements remove book functionality
+    // NB! after remove book, we have possibility to add book with same isbn
 }
 
 function findBook(library, isbn) {
-    // TODO return index of book with isbn or -1 if not found
     for (let i = 0; i < library.length; i++) {
         if(library[i].isbn === isbn){
             return i;
